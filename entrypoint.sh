77c7c7959811bdd60740fdafc5cce55cdb2405cb
Max Bauer <krummbar@pm.me>
2024-06-08 07:33:35 +0200

    optimize entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,38 @@ echo "::debug::INPUT_TESTSONLY='${INPUT_TESTSONLY}'"
 echo "::debug::INPUT_BAIL='${INPUT_BAIL}'"
 echo "::debug::DRY_RUN='${dry_run}'"
 
+# Exit script with status code and message
+#
+# $1 - Exit code
+# $2 - Message to be dumped before exiting
+function exit_with {
+  prefix="::notice"
+  if [[ "${1}" != "0" ]]; then
+    prefix="::error"
+  fi
+  echo "${prefix}::$2"
+  exit $1
+}
+
+# Takes absolute or relative paths and returns the absolute path value
+# based on the current working directory.
+#
+# $1 - The path of the file that should be resolved as absolute path
+#
+# Examples (assuming pwd=/home/user/bruno-action)
+#   absolute_path "out/bruno.log" => /home/user/bruno-action/out/bruno.log
+#   absolute_path "/out/bruno.log" => /out/bruno.log
+function absolute_path {
+  input_path="${1}"
+  origin_dir=$(pwd)
+  cd "$(dirname ${input_path})" &>/dev/null || exit_with 1 "the directory of the provided path '${input_path}' does not exist"
+  out_abs_path=$(pwd)
+  out_filename=$(basename ${input_path})
+  cd ${origin_dir}
+  echo "${out_abs_path}/${out_filename}"
+}
+
 # Parse input parameters
-# Change to provided working directory
-if [ -n "${INPUT_PATH}" ]; then
-  cd "${INPUT_PATH}" || exit 1
-fi
 if [ -n "${INPUT_FILENAME}" ]; then
   bru_args="${INPUT_FILENAME}"
 fi
@@ -31,7 +58,7 @@ if [ -n "${INPUT_ENV}" ]; then
 fi
 
 if [ -n "${INPUT_OUTPUT}" ]; then
-  bru_args="${bru_args} --output ${INPUT_OUTPUT}"
+  bru_args="${bru_args} --output $(absolute_path ${INPUT_OUTPUT})"
 fi
 
 if [ -n "${INPUT_OUTPUTFORMAT}" ]; then
@@ -58,27 +85,28 @@ if [ -n "${INPUT_ENVVARS}" ]; then
   done < <(echo -e "${INPUT_ENVVARS}")
 fi
 
+# Change to provided working directory
+if [ -n "${INPUT_PATH}" ]; then
+  cd "${INPUT_PATH}" || exit_with 1 "The provided bruno collection path '${INPUT_PATH}' does not exist"
+fi
+
 # Dump current working directory
 echo "::notice::collection directory: '$(pwd)'"
 
 # Only dump command if DRY_RUN is enabled and exit
 if [ -n "${dry_run}" ]; then
   echo "::notice::bru run ${bru_args}"
-  exit 0
+  exit_with 0 "Executed in dry mode, skipped executing bruno collection"
 fi
-
-
-echo "::notice::bru run ${bru_args}"
 
 # Execute 'bru run ...' and evaluate execution status
 if eval "bru run ${bru_args}"; then
-  echo "::notice::bru run succeeded."
   # Write outputs to the $GITHUB_OUTPUT file
   echo "success=true" >>"${GITHUB_OUTPUT}"
-  exit 0
+  exit_with 0 "bru run suceeded."
 else
-  echo "::warning::bru run failed with status: $?"
-  # Write outputs to the $GITHUB_OUTPUT file
+  bru_exit_code=$?
+  # Write outputs to warning $GITHUB_OUTPUT file
   echo "success=false" >>"${GITHUB_OUTPUT}"
-  exit 1
+  exit_with ${bru_exit_code} "bru run failed failed with status: ${bru_exit_code}."
 fi
